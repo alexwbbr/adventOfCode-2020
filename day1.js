b73860cace163af120d4465e17b557c8dc72507d
Alex Webber <alexwbbr@gmail.com>
2020-12-02 16:03:09 +0000

    day one puzzle

---
+++ day1.js
@@ -0,0 +1,9 @@
+const expenseReport = require('./expense-report')
+
+for (let reportItem of expenseReport) {
+    const remain = 2020 - reportItem;
+    if (expenseReport.includes(remain)) {
+        console.log(reportItem * remain + ' is the answer')
+        break;
+    }
+}
