769879b6cb6f1f137a9bf11f7675ad93f755ea9a
Alex Webber <alexwbbr@gmail.com>
2020-12-04 10:31:02 +0000

    added day 4 part 1

---
+++ day-4-part-1.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+fs.readFile('passport-list.txt', 'utf8', (error, res) => {
+    const passportLists = res.split('\n\n');
+
+    const totalValidPassports = passportLists.reduce((validPassports, passport) => {
+        const numberOfItems = passport.split('').filter((item) => item === ':').length;
+         if (numberOfItems === 8 || numberOfItems === 7 && !passport.includes('cid')) {
+             return validPassports+= 1;
+         }
+         return validPassports;
+    }, 0);
+    console.log(`Total valid passports: ${totalValidPassports}`);
+});
