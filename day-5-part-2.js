7fe3e727327d8fb1b226071090be29ef2cf53077
Alex Webber <alexwbbr@gmail.com>
2020-12-05 12:49:33 +0000

    added day 5 part 2

--- day-5-part-2.js
+++ day-5-part-2.js
@@ -1,18 +1,23 @@
 const fs = require('fs');
 
 fs.readFile('boarding-passes.txt', 'utf8', (error, res) => {
-    const boardingPasses = res.split('\n').filter((item) => item !== '');
-    let highestSeatId = 0;
+    const boardingPasses = res.split('\n').filter((item) => item !== "");
+    let seatList = [];
+    let mySeat = 0;
     boardingPasses.forEach((pass) => {
         const [full, rows, columns] = pass.split(/([FB]+)([RL]+)/);
         const rowNumber = calculatePosition(0, 127, 'F', rows);
         const colNumber = calculatePosition(0, 7, 'L', columns);
         const seatId = (rowNumber * 8) + colNumber;
-        if (seatId > highestSeatId) {
-            highestSeatId = seatId;
-        }
+        seatList.push(seatId);
     });
-    console.log(`The highest seat ID is:${highestSeatId}`)
+    seatList.sort((a, b) => a - b).forEach((seat, index) => {
+        if (seat + 1 === seatList[index + 1] || index === seatList.length - 1) {
+            return;
+        }
+        mySeat = seat + 1;
+    });
+    console.log(`My seat ID is ${mySeat}`)
 });
 
 function calculatePosition(lowerLimit, upperLimit, lowerChar, string) {
